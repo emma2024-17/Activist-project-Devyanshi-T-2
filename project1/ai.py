98fa45e45063aea8c81212aa225e00d8c212e227
emma2024-17 <emma17secret@gmail.com>
2024-07-02 15:16:13 +0530

    Add files via upload

---
+++ project1/ai.py
@@ -0,0 +1,14 @@
+class User():
+
+    def __init__(self):
+
+
+user_1 = User("00 1")
+user_1.id = "001"
+user_1.username = "angela"
+
+print(user_1.username)
+
+user_2 = User()
+user2_id = "002 "
+user_2.username = "jack"
